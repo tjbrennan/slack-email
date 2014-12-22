85ad036c007be5bef1136e6613ef0dc8981ee9c8
Taylor Brennan <taylorjbrennan@gmail.com>
2014-12-21 18:44:48 -0800

    upload file to slack

--- app.js
+++ app.js
@@ -2,15 +2,25 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mailHook = require('./mailhook');
 
-
 var app = express();
 var port = process.env.PORT || 3000;
+
 
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// set locals
+app.use(function (req, res, next) {
+  res.locals.slack = {
+    apiToken : process.env.SLACK_API_TOKEN,
+    channels : process.env.SLACK_CHANNELS
+  }
 
-// test route
+  next();
+});
+
+
+// email route
 app.post('/email', mailHook);
 
 
--- mailhook.js
+++ mailhook.js
@@ -1,14 +1,86 @@
 var MailParser = require('mailparser').MailParser; // https://github.com/andris9/mailparser
+var request = require('request');
+var sanitize = require('sanitize-filename');
+var through = require('through');
 
 module.exports = function (req, res, next) {
   var mailparser = new MailParser({
-    debug : true,
+    debug : false,
     streamAttachments : false
   });
 
+  // mailparser.on('attachment', function (attachment) {
+  //   var formData = {
+  //     token : res.locals.slack.apiToken,
+  //     file : ,
+  //     filename : attachment.generatedFileName,
+  //     title : attachment.fileName,
+  //     channels : res.locals.slack.channels
+  //   };
+  //
+  //   request({
+  //     uri : 'https://slack.com/api/files.upload',
+  //     method : 'POST',
+  //     formData : formData
+  //   }, function (error, response, body) {
+  //     if (error) {
+  //       return next(error);
+  //     }
+  //
+  //     console.log(response.statusCode, body);
+  //
+  //   });
+  // });
+
   mailparser.on('end', function (mailObject) {
     console.dir(mailObject);
-    res.status(200).end();
+
+    request({
+      uri : 'https://slack.com/api/files.upload',
+      method : 'POST',
+      formData : {
+        token : res.locals.slack.apiToken,
+        content : mailObject.text,
+        filename : sanitize(mailObject.subject, { replacement : '_' }),
+        title : mailObject.subject,
+        initial_comment : 'To: ' + mailObject.headers.to +
+                          '\nFrom: ' + mailObject.headers.from +
+                          '\nSubject: ' + mailObject.subject,
+        channels : res.locals.slack.channels
+      }
+    }, function (error, response, body) {
+      if (error) {
+        return next(error);
+      }
+
+      console.log(response.statusCode, body);
+
+    });
+
+    mailObject.attachments.forEach(function (attachment) {
+      console.log(attachment.content);
+      var formData = {
+        token : res.locals.slack.apiToken,
+        file : attachment.content,
+        filename : attachment.generatedFileName,
+        title : attachment.fileName,
+        channels : res.locals.slack.channels
+      };
+
+      request({
+        uri : 'https://slack.com/api/files.upload',
+        method : 'POST',
+        formData : formData
+      }, function (error, response, body) {
+        if (error) {
+          return next(error);
+        }
+
+        console.log(response.statusCode, body);
+
+      });
+    })
+
   });
 
   mailparser.write(req.body.message);
